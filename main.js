f684b9d8d29270de5b306a13b528ba6c8e60ee16
Andrew Stephens <andrewmediaprod@gmail.com>
2021-07-27 22:50:25 -0400

    Add images and final content

--- main.js
+++ main.js
@@ -3,22 +3,28 @@ var ctx;
 var rotationStartTime;
 var images = [];
 var imageIndex = 0;
+var width;
+var height;
 
 const IMAGE_URLS = [
-  "https://via.placeholder.com/500.png/09f/fff?text=1",
-  "https://via.placeholder.com/500.png/f9f/fff?text=2",
-  "https://via.placeholder.com/500.png/90f/fff?text=3",
+  "/Images/slideshow1a.jpg",
+  "/Images/slideshow2.jpg",
+  "/Images/slideshow3.jpg",
 ];
 const a = 2 * Math.PI / 6;
 const ADVANCE_INTERVAL_MS = 10000;
 const ROTATION_SPEED_MS = 1000;
 const ROTATION_AMOUNT = Math.PI / 3;
-const BORDER_WIDTH = 10;
+const BORDER_WIDTH_DIVISOR = 40;
 
 function init() {
   canvas = document.getElementById('photoshow');
   ctx = canvas.getContext('2d');
-  
+
+  // Set canvas bound vars on load and on any resize.
+  window.addEventListener('resize', setSize, true);
+  setSize();
+ 
   // Load all images from URL.
   for (let i = 0; i < IMAGE_URLS.length; i++) {
     const url = IMAGE_URLS[i];
@@ -27,6 +33,7 @@ function init() {
     images[i] = img;
   }
 
+  // Call drawing function to start infinite loop.
   window.requestAnimationFrame(draw);
 
   // Set a periodic interval to advance the slideshow.
@@ -40,13 +47,39 @@ function init() {
   }, ADVANCE_INTERVAL_MS);
 }
 
+function setSize() {
+  // Get the device pixel ratio, falling back to 1.
+  let dpr = window.devicePixelRatio || 1;
+  // Get current size of the canvas.
+  let rect = canvas.getBoundingClientRect();
+
+  // Increase the actual size of our canvas (in HTML attributes).
+  // All size calculations assume a square canvas.
+  canvas.width = rect.width * dpr;
+  canvas.height = rect.width * dpr;
+
+  // Ensure all drawing operations are scaled.
+  ctx.scale(dpr, dpr);
+
+  // Scale everything down using CSS
+  canvas.style.width = rect.width + 'px';
+  canvas.style.height = rect.width + 'px';
+
+  // Save visible measurements for drawing later.
+  width = rect.width;
+  height = rect.width;
+
+  console.log("Setting size", rect.width, rect.width, canvas.width, canvas.height)
+}
+
 function draw() {
-  const centerX = canvas.width / 2;
-  const centerY = canvas.height / 2;
-  const radius = (canvas.width / 2) - BORDER_WIDTH;
+  const centerX = width / 2;
+  const centerY = height / 2;
+  const borderWidth = width / BORDER_WIDTH_DIVISOR;
+  const radius = (width / 2) - borderWidth;
 
   ctx.save();
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, width, height);
 
   let rotationOffset = 0;
   let imageOpacity = 1.0;
@@ -67,13 +100,13 @@ function draw() {
   ctx.clip();
 
   ctx.globalAlpha = imageOpacity;
-  ctx.drawImage(images[imageIndex], 0, 0, canvas.width, canvas.height)
+  ctx.drawImage(images[imageIndex], 0, 0, width, height)
 
   // Reset context since we don't want the clipping path to apply any more.
   ctx.restore();
 
-  ctx.save();
-  ctx.lineWidth = BORDER_WIDTH;
+  ctx.saveBORDER_WIDTH_DIVISOR;
+  ctx.lineWidth = borderWidth;
   ctx.stroke();
   ctx.restore();
 
